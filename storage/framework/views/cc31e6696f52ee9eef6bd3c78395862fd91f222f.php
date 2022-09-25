3abe5983d114801a1d6a7bba802a8571fe5a6a49
Mohamadreza Rezaei <clash.trial@gmail.com>
2022-09-26 00:47:59 +0330

    forget adding some files from last commit

---
+++ storage/framework/views/cc31e6696f52ee9eef6bd3c78395862fd91f222f.php
@@ -0,0 +1,235 @@
+<?php $__env->startSection('head-tag'); ?>
+<title>برند</title>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+      <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
+      <li class="breadcrumb-item font-size-12"> <a href="#">برند</a></li>
+      <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد برند</li>
+    </ol>
+  </nav>
+
+
+  <section class="row">
+    <section class="col-12">
+        <section class="main-body-container">
+            <section class="main-body-container-header">
+                <h5>
+                  ایجاد برند
+                </h5>
+            </section>
+
+            <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                <a href="<?php echo e(route('admin.market.brand.index')); ?>" class="btn btn-info btn-sm">بازگشت</a>
+            </section>
+
+            <section>
+                <form action="<?php echo e(route('admin.market.brand.update', $brand->id)); ?>" method="post" id="form" enctype="multipart/form-data">
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field('PUT'); ?>
+                    <section class="row">
+
+                        <section class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">نام اصلی برند</label>
+                                <input type="text" class="form-control form-control-sm" name="original_name" value="<?php echo e(old('original_name', $brand->original_name)); ?>">
+                            </div>
+                            <?php $__errorArgs = ['original_name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                <strong>
+                                    <?php echo e($message); ?>
+
+                                </strong>
+                            </span>
+                            <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                        </section>
+
+                        <section class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">نام فارسی برند</label>
+                                <input type="text" class="form-control form-control-sm" name="persian_name" value="<?php echo e(old('persian_name', $brand->persian_name)); ?>">
+                            </div>
+                            <?php $__errorArgs = ['persian_name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                <strong>
+                                    <?php echo e($message); ?>
+
+                                </strong>
+                            </span>
+                            <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                        </section>
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for="tags">تگ ها</label>
+                                <input type="hidden" class="form-control form-control-sm" name="tags" id="tags"
+                                    value="<?php echo e(old('tags', $brand->tags)); ?>">
+                                <select class="select2 form-control form-control-sm" id="select_tags" multiple>
+
+                                </select>
+                            </div>
+                            <?php $__errorArgs = ['tags'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        <?php echo e($message); ?>
+
+                                    </strong>
+                                </span>
+                            <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                        </section>
+
+
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for="status">وضعیت</label>
+                                <select name="status" id="" class="form-control form-control-sm" id="status">
+                                    <option value="0" <?php if(old('status', $brand->status) == 0): ?> selected <?php endif; ?>>غیرفعال</option>
+                                    <option value="1" <?php if(old('status', $brand->status) == 1): ?> selected <?php endif; ?>>فعال</option>
+                                </select>
+                            </div>
+                            <?php $__errorArgs = ['status'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        <?php echo e($message); ?>
+
+                                    </strong>
+                                </span>
+                            <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                        </section>
+
+
+
+
+                        <section class="col-12 col-md-12">
+                            <div class="form-group">
+                                <label for="">تصویر برند</label>
+                                <input type="file" class="form-control form-control-sm" name="logo">
+                            </div>
+                            <?php $__errorArgs = ['logo'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                <strong>
+                                    <?php echo e($message); ?>
+
+                                </strong>
+                            </span>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                        </section>
+
+                        <section class="row">
+                            <?php
+                                $number = 1;
+                                ?>
+                            <?php $__currentLoopData = $brand->logo['indexArray']; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $value): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <section class="col-md-<?php echo e(6 / $number); ?>">
+                                <div class="form-check">
+                                    <input type="radio" class="form-check-input" name="currentImage" value="<?php echo e($key); ?>" id="<?php echo e($number); ?>" <?php if($brand->logo['currentImage'] == $key): ?> checked <?php endif; ?>>
+                                    <label for="<?php echo e($number); ?>" class="form-check-label mx-2">
+                                        <img src="<?php echo e(asset($value)); ?>" class="w-100" alt="">
+                                    </label>
+                                </div>
+                            </section>
+                            <?php
+                            $number++;
+                        ?>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+                        </section>
+
+
+
+                        <section class="col-12">
+                            <button class="btn btn-primary btn-sm">ثبت</button>
+                        </section>
+                    </section>
+                </form>
+            </section>
+
+        </section>
+    </section>
+</section>
+
+<?php $__env->stopSection(); ?>
+
+
+
+<?php $__env->startSection('script'); ?>
+
+    <script src="<?php echo e(asset('admin-assets/ckeditor/ckeditor.js')); ?>"></script>
+    <script>
+        CKEDITOR.replace('description');
+    </script>
+
+    <script>
+        $(document).ready(function () {
+            var tags_input = $('#tags');
+            var select_tags = $('#select_tags');
+            var default_tags = tags_input.val();
+            var default_data = null;
+
+            if(tags_input.val() !== null && tags_input.val().length > 0)
+            {
+                default_data = default_tags.split(',');
+            }
+
+            select_tags.select2({
+                placeholder : 'لطفا تگ های خود را وارد نمایید',
+                tags: true,
+                data: default_data
+            });
+            select_tags.children('option').attr('selected', true).trigger('change');
+
+
+            $('#form').submit(function ( event ){
+                if(select_tags.val() !== null && select_tags.val().length > 0){
+                    var selectedSource = select_tags.val().join(',');
+                    tags_input.val(selectedSource)
+                }
+            })
+        })
+    </script>
+
+<?php $__env->stopSection(); ?>
+
+
+
+<?php echo $__env->make('admin.layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\CODEX\mars-shop\resources\views/admin/market/brand/edit.blade.php ENDPATH**/ ?>
